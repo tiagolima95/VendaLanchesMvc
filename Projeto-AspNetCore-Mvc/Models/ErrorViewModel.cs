3781b934d062fd2bc1c11dd33cdcd0580e5971a9
Tiago Lima <tiagoo.lima15@gmail.com>
2022-07-24 15:42:38 -0300

    Project created

---
+++ Projeto-AspNetCore-Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_AspNetCore_Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
